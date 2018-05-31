e55223496b21bc3cda1ccd04b4effa98de5e3341
KURAOKAMI\yukipastelcat <kirill.sedunov@roskvartal.ru>
2018-05-31 18:28:17 +0300

    Добавил логику омнибокса (пока без синонимов)

--- src/bem/blocks/search/search.js
+++ src/bem/blocks/search/search.js
@@ -1,3 +1,27 @@
-let initOmnibox = function () {
-
+let search = function (regex) {
+    document.querySelectorAll('.entries-table__publication').forEach(publication => {
+        if (!regex.test(publication.dataset.tags)) {
+            publication.classList.add('entries-table__publication_hidden');
+        }
+        else {
+            publication.classList.remove('entries-table__publication_hidden');
+        }
+    });
+    document.querySelectorAll('.tag').forEach(tag => {
+        if (!regex.test(tag.getAttribute('title'))) {
+            tag.classList.add('tag_hidden');
+        }
+        else {
+            tag.classList.remove('tag_hidden');
+        }
+    });
 }
+
+let initOmnibox = function () {
+    document.querySelector(`.search input[type='search']`).addEventListener('input', function () {
+        let regex = new RegExp(this.value.replace(/ /g, '|'), 'i');
+        search(regex);
+    });
+}
+
+exports.initOmnibox = initOmnibox;
--- src/bem/bundles/main.decl.js
+++ src/bem/bundles/main.decl.js
@@ -1,5 +1,7 @@
 let entriesTable = require('../blocks/entries-table/entries-table');
 let tag = require('../blocks/tag/tag');
+let search = require('../blocks/search/search')
 
 entriesTable.makeSortable();
 tag.tagSearchable();
+search.initOmnibox();
