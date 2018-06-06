aad943fd9672be83f6574b7a350e0be36fbbe882
KURAOKAMI\yukipastelcat <kirill.sedunov@roskvartal.ru>
2018-06-06 19:18:16 +0300

    Тестирование + missing пакет

--- tests/bibtex.js
+++ tests/bibtex.js
@@ -5,6 +5,7 @@ let bibtexService = require('../src/services/bibtexService');
 
 describe('bibtexService', function () {
     let references = fs.readFileSync('tests/bibtex/references.bib');
+    let incorrectReferences = fs.readFileSync('tests/bibtex/references_incorrect.bib');
     describe('Converts BibTeX entries to JavaScript object', function () {
         let object = bibtexService.bibtexToJson(references);
         it(`Returned object is not empty`, function () {
@@ -17,6 +18,18 @@ describe('bibtexService', function () {
         it(`Each publication in object has 'entry_key' property`, function () {
             for (publication in object)
                 object[publication].should.have.property('entry_key');
+        });
+    });
+    describe('Error handling', function () {
+        let object = bibtexService.bibtexToJson(incorrectReferences);
+        it('Should return parsed contents and incomplete flag, when BibTeX base is corrupted', function () {
+            let incompleteFlag = false;
+            for (publication in object)
+                object[publication].should.have.property('bib_type');
+                object[publication].should.have.property('entry_key');
+                if (object[publication].incomplete)
+                    incompleteFlag = true;
+            incompleteFlag.should.be.true;
         });
     });
 });
