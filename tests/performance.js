94d743c96d79d90c7c6ec8cd1f1004ce92c02115
KURAOKAMI\yukipastelcat <kirill.sedunov@roskvartal.ru>
2018-06-07 15:15:24 +0300

    Добавил бенчмарк

--- tests/performance.js
+++ tests/performance.js
@@ -1,7 +1,8 @@
 let chai = require('chai');
 let should = chai.should();
 let fs = require('fs');
-let bibtexService = require('../src/services/bibtexService');
+let bibtexService = require('../src/services/bibtexService'),
+    synonymService = require('../src/services/synonymService');
 
 describe('bibtexService performance', function () {
     let references = fs.readFileSync('tests/bibtex/references_formatinsensitive.bib').toString();
@@ -76,12 +77,112 @@ describe('bibtexService performance', function () {
         this.timeout(60000);
         references = incrementAndParse(references);
     });
+    let incrementAndParse = function(refs) {
+        refs = refs.repeat(2);
+        let object = bibtexService.bibtexToJson(refs);
+        object.should.be.an('object');
+        return refs;
+    }
 });
 
-
-function incrementAndParse(refs) {
-    refs = refs.repeat(2);
-    let object = bibtexService.bibtexToJson(refs);
-    object.should.be.an('object');
-    return refs;
-}
+describe('synonymService performance', function () {
+    let synonyms = [];
+    let count = 1;
+    it('1 synonym', function () {
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('2 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('4 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('8 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('16 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('32 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('64 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('128 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('256 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('512 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('1024 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('2048 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('4096 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('8192 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('16384 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('32768 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('65536 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    it('131072 synonyms', function () {
+        count = count * 2;
+        let synonym = incrementAndSearch(count);
+        chai.expect(synonym).to.have.property('test');
+    });
+    let incrementAndSearch = function(count) {
+        for (var i = 0; i < count; i++) {
+            synonyms.push(`synonym${i}|synonym${i + 1}`);
+        }
+        let synonym = synonymService.findSynonym(new RegExp('synonym' + (count - 1), 'i'), synonyms);
+        synonyms = [];
+        return synonym;
+    }
+});
