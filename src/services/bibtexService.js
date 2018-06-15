83cee46c0c19761b338813692798315e3c4d864f
KURAOKAMI\yukipastelcat <kirill.sedunov@roskvartal.ru>
2018-06-15 14:28:44 +0300

    Работа с kBibTeX

--- src/services/bibtexService.js
+++ src/services/bibtexService.js
@@ -1,6 +1,9 @@
 const converter = require('biblatex-csl-converter');
 function bibtexToJson (blob) {
-    let fileString = blob.toString();
+    let fileString = blob
+                    .toString()
+                    .replace(/\blocalfile\b/igm, 'file');
+    console.log(fileString);
     let json;
     let parser = new converter.BibLatexParser(fileString, { processUnexpected: true });
     json = parser.output;
